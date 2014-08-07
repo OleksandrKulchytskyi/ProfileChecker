9184920c2a8e8851840b59dffcb563df295a4352
OleksandrKulchytskyi <sashaas@inbox.ru>
2014-08-07 16:20:44 +0300

    New test method for some specific logic has been created

--- DNSProfileChecker.UnitTest/WorkflowsUnitTests.cs
+++ DNSProfileChecker.UnitTest/WorkflowsUnitTests.cs
@@ -2,7 +2,11 @@
 using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using DNSProfileChecker.Common;
+using DNSProfileChecker.Common.Extension;
 using DNSProfileChecker.Common.Implementation;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
 
 namespace DNSProfileChecker.UnitTest
 {
@@ -52,6 +56,112 @@ namespace DNSProfileChecker.UnitTest
 					{
 						assignLogger(item, logger);
 					}
+				}
+			}
+		}
+
+		[TestMethod]
+		public void TestProfileFixMethod()
+		{
+			PerformProfileCleanup(@"\\DEV804\DragonUsers2\abdalahmr");
+		}
+
+		private void PerformProfileCleanup(string userProfile)
+		{
+			if (!ConfigurationManager.AppSettings.AllKeys.Contains("enableProfileCheck"))
+				return;
+
+			if (ConfigurationManager.AppSettings["enableProfileCheck"].ToLower() != "true")
+				return;
+
+			ILogger logger = new DebugLogger();
+			IProfileAssurance profileAssert = new ProfileAssurance();
+			IFileFactory filesFactory = new DNSProfileChecker.Common.Factories.FileFactory();
+			IWorkflowProvider workflowsProvider = new XmlWorkflowProvider();
+			workflowsProvider.Parameters = Path.Combine(System.AppDomain.CurrentDomain.BaseDirectory, "Workflows.xml");
+
+			List<IProfileWorkflow> workflows = null;
+			try
+			{
+				workflows = workflowsProvider.Initialize();
+				foreach (IProfileWorkflow workflow in workflows)
+				{
+					assignLoggerAndStateToWorkflow(workflow, logger);
+				}
+			}
+			catch (Exception ex)
+			{
+				logger.LogData(LogSeverity.Error, "Unable to initialize workflows.", ex);
+				return;
+			}
+
+			bool isErrorOccurred = false;
+			Exception error = null;
+			bool isProfileCorrect = true;
+
+			logger.LogData(LogSeverity.UI, string.Format("Begin to process {0} DNS profile.", userProfile), null);
+			foreach (IProfileWorkflow workflow in workflows)
+			{
+				try
+				{
+					isErrorOccurred = false;
+					error = null;
+					workflow.Execute(userProfile);
+				}
+				catch (Exception ex)
+				{
+					isErrorOccurred = true;
+					error = ex;
+				}
+
+				if (isErrorOccurred)
+				{
+					isProfileCorrect = false;
+					logger.LogData(LogSeverity.Error, error.Message, error);
+					continue;
+				}
+
+				if (workflow.IsImportant)
+				{
+					if (workflow.State == WorkflowStates.Failed || workflow.State == WorkflowStates.Exceptional || workflow.State == WorkflowStates.Warn)
+					{
+						if (workflow.State != WorkflowStates.Warn && workflow.Description.IsNotNullOrEmpty())
+							logger.LogData(LogSeverity.Error, workflow.Description, null);
+						isProfileCorrect = false;
+						continue;
+					}
+				}
+				else
+				{
+					if (workflow.State == WorkflowStates.Failed || workflow.State == WorkflowStates.Exceptional || workflow.State == WorkflowStates.Warn)
+					{
+						logger.LogData(LogSeverity.Warn, workflow.Description, null);
+						isProfileCorrect = false;
+						continue;
+					}
+				}
+
+				isProfileCorrect = workflow.IsProfileMatchState(WorkflowStates.Success);
+			}//end loop for workwflows
+
+			if (isProfileCorrect)
+				logger.LogData(LogSeverity.Success, string.Format("Profile {0} has been checked with no error(s).", userProfile), null);
+			else
+				logger.LogData(LogSeverity.UI, string.Format("Profile [{0}] has been verified.", userProfile), null);
+		}
+
+		private void assignLoggerAndStateToWorkflow(IProfileWorkflow wf, ILogger logger)
+		{
+			if (logger != null)
+			{
+				wf.Logger = logger;
+				wf.IsSimulationMode = false;
+				if (wf.SubsequentWorkflows == null)
+					return;
+
+				foreach (var item in wf.SubsequentWorkflows)
+				{
+					assignLoggerAndStateToWorkflow(item, logger);
 				}
 			}
 		}
