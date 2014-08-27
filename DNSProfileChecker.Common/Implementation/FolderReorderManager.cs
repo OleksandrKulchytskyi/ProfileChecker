c1ae770f58649d75054735a6947da9b8ebe9a0cb
OleksandrKulchytskyi <sashaas@inbox.ru>
2014-08-27 16:09:40 +0300

    Fixed issue related to the reordering workflow

--- DNSProfileChecker.Common/Implementation/FolderReorderManager.cs
+++ DNSProfileChecker.Common/Implementation/FolderReorderManager.cs
@@ -22,7 +22,7 @@ namespace DNSProfileChecker.Common.Implementation
 		{
 			Ensure.Argument.NotNull(folders, "folders parameter cannot be a null.");
 			bool result = true;
-			bool isSequenseCorreted = false;
+			//bool isSequenseCorreted = false;
 			if (folders.Length == 0)
 				return result;
 #if DEBUG
@@ -33,55 +33,29 @@ namespace DNSProfileChecker.Common.Implementation
 
 			List<Exception> excList = new List<Exception>();
 			int foldersCount = folders.Length;
-			while (!isSequenseCorreted)
+
+			for (int i = 0; i < foldersCount; i++)
 			{
-				if (foldersCount == 1)
+				DirectoryInfo current = folders[i];
+				if (String.Compare(current.Name, expected[i]) != 0)
 				{
-					DirectoryInfo newDi = new DirectoryInfo(Path.Combine(parentFolder, "session1"));
-					newDi.Create();
-					newDi.CopyFrom(folders[0]);
-					folders[0].Delete(true);
-				}
-				else
-				{
-					for (int i = foldersCount - 1; i >= 0; i--)
+					DirectoryInfo newDi = new DirectoryInfo(Path.Combine(parentFolder, expected[i]));
+					if (!newDi.Exists)
 					{
-						DirectoryInfo current = folders[i];
-						int number = int.Parse(current.Name.Remove(0, "session".Length));
-						if (i > 0)
+						try
 						{
-							DirectoryInfo nextDir = folders[i - 1];
-							int lowerNumber = int.Parse(nextDir.Name.Remove(0, "session".Length));
-							if (number - 1 != lowerNumber)
+							if (!useMove)
 							{
-								DirectoryInfo newDi = new DirectoryInfo(Path.Combine(parentFolder, "session" + (lowerNumber + 1).ToString()));
-								if (!newDi.Exists)
-								{
-									try
-									{
-										if (!useMove)
-										{
-											newDi.Create();
-											newDi.CopyFrom(current);
-											current.Delete(true);
-										}
-										else
-											newDi.Move(current);
-									}
-									catch (Exception exc) { excList.Add(exc); }
-									finally
-									{
-										try { if (newDi.Exists) newDi.Delete(true); }
-										catch { }
-									}
-								}
-								break;
+								newDi.Create();
+								newDi.CopyFrom(current);
+								current.Delete(true);
 							}
+							else
+								newDi.Move(current);
 						}
+						catch (Exception exc) { excList.Add(exc); }
 					}
 				}
-				folders = new DirectoryInfo(parentFolder).GetDirectories("session*", SearchOption.TopDirectoryOnly).OrderBy(f => int.Parse(f.Name.Remove(0, "session".Length))).ToArray();
-				isSequenseCorreted = expected.Except(folders.Select(f => f.Name)).Any() == false;
 			}
 
 #if DEBUG
